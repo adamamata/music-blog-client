a4ba720f0f0eb00ea7529bf336c6f753dba29b97
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-11-02 12:47:30 +0100

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+//Imports
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+//Pages
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+    {/* Routes to pages */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
